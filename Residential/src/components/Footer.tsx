ec7a6eebdf3ebcd90995a919f35b120a16b1f01b
umerfarooqlaghari <mumerfarooqlaghari@gmail.com>
2025-07-23 00:54:22 +0500

    updated info

--- Residential/src/components/Footer.tsx
+++ Residential/src/components/Footer.tsx
@@ -96,13 +96,13 @@ We&apos;re a trusted real estate company focused on helping property owners find
                 <svg className="w-5 h-5 text-blue-400 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
-                <span className="text-blue-200">(555) 123-4567</span>
+                <span className="text-blue-200">(561) 889-6041</span>
               </div>
               <div className="flex items-center">
                 <svg className="w-5 h-5 text-blue-400 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 4.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
-                <span className="text-blue-200">info@residentialrehab.com</span>
+                <span className="text-blue-200">sakes@residentialrehabgroup.com</span>
               </div>
               <div className="flex items-center">
                 <svg className="w-5 h-5 text-blue-400 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
